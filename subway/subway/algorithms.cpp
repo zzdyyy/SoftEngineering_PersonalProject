e6afa43d323d963b82d2f735ecbd1e9585e86f0e
zzdyyy <zzdyyy@qq.com>
2016-09-14 16:39:38 +0800

    Implemented a function to make up a adjust matrix.

--- subway/subway/algorithms.cpp
+++ subway/subway/algorithms.cpp
@@ -4,11 +4,32 @@
 //AdjustMatrix to accelerate the computing
 //In the FindShortestPath, it was used to store the whole metro graph
 //In the FewestTranser, it may store the metro graph of specific MetroLines
-unsigned char **AdjustMatrix;
+unsigned char *AdjustMatrix = nullptr;
+size_t StationNum = 0;	//the width and height of AdjustMatrix
+//a inline function to access AdjustMatrix as an 2-d array
+static inline unsigned char &ajm(size_t i, size_t j) { return *(AdjustMatrix + i * StationNum + j); }
 
 void FillInAdjustMatrix()
 {
-	//TODO
+	//get an array dynamically
+	StationNum = StationList.size();
+	AdjustMatrix = new unsigned char[StationNum*StationNum];
+	
+	//fill the ajm with 0
+	unsigned char *p = AdjustMatrix;
+	for (size_t i = 0; i < StationNum*StationNum; ++i)
+		*p++ = '\0';
+
+	for (size_t id = 0; id < StationNum; ++id)	//for each row in ajm
+	{
+		Station s = GetStation(id);
+		for (Station::Adjoins_t::iterator iter = s.adjoins.begin();
+			iter != s.adjoins.end();
+			++iter)
+		{
+			ajm(id, iter->StationId) = '\01';
+		}
+	}
 }
 
 void StationQuery()
@@ -37,7 +58,8 @@ void StationQuery()
 
 void ShortestPath(const string &station1, const string &station2)
 {
-	cout << "shortest path:" << station1 << station2 << endl;
+	FillInAdjustMatrix();
+	cout << "shortest path:" << station1 << station2 << endl; system("pause");
 }
 
 void FewestTransferPath(const string &station1, const string &station2)
